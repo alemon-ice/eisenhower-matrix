7ce2e6c16668ea999075e208d5f6866146e098ee
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-01-04 22:14:05 -0300

    Make schedule listing responsive

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,56 @@
-import { useState, useCallback, useEffect } from 'react'
-import Head from 'next/head'
-import { FaExternalLinkAlt, FaSearch } from 'shared/utils/external-components'
+import { useState, useCallback, useEffect } from 'react';
+import Head from 'next/head';
+import { FaExternalLinkAlt, FaSearch } from 'shared/utils/external-components';
 
-import styles from 'shared/styles/Home.module.css'
-import BoardModel from 'shared/models/Board'
-import { IBoard } from 'shared/interfaces/board'
-import { fetchBoardService } from 'shared/services/board.service'
-import { ToastContainer, BiRefresh } from 'shared/utils/external-components'
-import { Matrix } from 'shared/components/organisms'
-import { ListSchedules, ListCards } from 'shared/components/molecules'
+import styles from 'shared/styles/Home.module.css';
+import BoardModel from 'shared/models/Board';
+import { IBoard } from 'shared/interfaces/board';
+import { fetchBoardService } from 'shared/services/board.service';
+import { ToastContainer, BiRefresh } from 'shared/utils/external-components';
+import { Matrix } from 'shared/components/organisms';
+import { ListSchedules, ListCards } from 'shared/components/molecules';
 
 export default function Home() {
-  const [board, setBoard] = useState<BoardModel>()
-  const [refreshing, setRefreshing] = useState(false)
+  const [board, setBoard] = useState<BoardModel>();
+  const [refreshing, setRefreshing] = useState(false);
 
   function fetchBoard() {
     (async () => {
-      const boardResponse: IBoard = await fetchBoardService()
-      setBoard(new BoardModel(boardResponse))
-    })()
+      const boardResponse: IBoard = await fetchBoardService();
+      setBoard(new BoardModel(boardResponse));
+    })();
   }
 
   function changeList(index: number) {
-    setBoard(board.selectMatrixList(index))
+    setBoard(board.selectMatrixList(index));
   }
 
   function refresh() {
     (async () => {
       try {
-        setRefreshing(true)
-        fetchBoard()
+        setRefreshing(true);
+        fetchBoard();
       } catch (err) {
-        console.error({ refreshError: err })
+        console.error({ refreshError: err });
       } finally {
         setTimeout(() => {
-          setRefreshing(false)
-        }, 2000)
+          setRefreshing(false);
+        }, 2000);
       }
-    })()
+    })();
   }
 
-  const handleChangeList = useCallback(changeList, [board])
-  const handleRefresh = useCallback(fetchBoard, [])
+  const handleChangeList = useCallback(changeList, [board]);
+  const handleRefresh = useCallback(fetchBoard, []);
 
-  useEffect(fetchBoard, [])
+  useEffect(fetchBoard, []);
 
   if (!board)
     return (
       <div className={styles.main}>
         <FaSearch size={25} className={styles.loading} />
       </div>
-    )
+    );
 
   return (
     <div className={styles.container}>
@@ -68,25 +68,39 @@ export default function Home() {
           </a>
         </h1>
         <span className={styles.quote}>
-          O que é importante raramente é urgente e o que é urgente raramente é importante
+          O que é importante raramente é urgente e o que é urgente raramente é
+          importante
         </span>
         <span className={styles.reference}>
           – Dwight Eisenhower, 34º Presidente Americano
         </span>
 
         <div className={styles.content}>
-          {/* <ListSchedules list={board.schedulesList} refresh={handleRefresh} /> */}
-          <Matrix lists={board.matrixList} handleChangeList={handleChangeList} />
+          <ListSchedules list={board.schedulesList} refresh={handleRefresh} />
+          <Matrix
+            lists={board.matrixList}
+            handleChangeList={handleChangeList}
+          />
           {board.selectedMatrixList ? (
-            <ListCards list={board.selectedMatrixList} listIndex={board.selectedMatrixIndex} />
+            <ListCards
+              list={board.selectedMatrixList}
+              listIndex={board.selectedMatrixIndex}
+            />
           ) : null}
         </div>
 
-        <button className={styles.refreshButton} onClick={refresh} disabled={refreshing}>
-          <BiRefresh className={refreshing && styles.refreshingPage} size={20} />
+        <button
+          className={styles.refreshButton}
+          onClick={refresh}
+          disabled={refreshing}
+        >
+          <BiRefresh
+            className={refreshing && styles.refreshingPage}
+            size={20}
+          />
         </button>
       </main>
       <ToastContainer />
     </div>
-  )
+  );
 }
